015fb896b7adc196da1709135b66e7fddaf0da3a
henry <henry-schulz@hotmail.de>
2021-05-09 18:24:59 +0200

    Large Class

--- src/Movie.java
+++ src/Movie.java
@@ -1,7 +1,4 @@
-public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
+public class Movie extends MovieSuper {
 
     private String title;
     private int priceCode;
