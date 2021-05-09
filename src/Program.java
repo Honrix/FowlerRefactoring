1aee86a29424ff168fa145ecf7e8c91982a53ba3
henry <henry-schulz@hotmail.de>
2021-05-09 18:15:26 +0200

    Duplicated Code

--- src/Program.java
+++ src/Program.java
@@ -5,27 +5,31 @@ import java.util.*;
  * by running "java -jar JarFile.jar" in the command line.
  * Java console applications can not be previewed in the Compilr IDE, only applets can.
  */
-public class Program
-{
+public class Program {
     /**
      * This is the main entry point for the application
      */
-    
-    
-    public static void main(String args[]) 
+
+    public static void main(String args[])
     {
         String result;
         System.out.println("Welcome to the Movie Store");
-        Movie m1 = new Movie("movie1", 1);
-        Movie m2 = new Movie("movie2", 2);
-        Rental r1 = new Rental(m1, 10);
-        Rental r2 = new Rental(m2, 5);
+
         Customer c1 = new Customer("joe");
-        c1.addRental(r1);   c1.addRental(r2);   
+
+        for(int i = 0; i < 2; i++){
+            Movie m = new Movie("movie" + i, (i+1));
+            Rental r = new Rental(m, (i < 1? 10 : 5));
+            c1.addRental(r);
+
+        }
+
         System.out.println("Let's get the Statement");
+
         result = c1.statement();
         System.out.println(result);
     }
+
 }
 
 
