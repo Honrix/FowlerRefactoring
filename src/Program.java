c84d87ddb5a39e9808186d57130e45e01e2ae6bb
henry <henry-schulz@hotmail.de>
2021-05-09 18:28:49 +0200

    Divergent Change

--- src/Program.java
+++ src/Program.java
@@ -1,17 +1,11 @@
 import java.lang.*;
-import java.util.*;
-/**
- * Note that Java console applications need to be run through the java runtime
- * by running "java -jar JarFile.jar" in the command line.
- * Java console applications can not be previewed in the Compilr IDE, only applets can.
- */
+
 public class Program {
-    /**
-     * This is the main entry point for the application
-     */
 
     public static void main(String args[])
     {
+        int[] numbers = {10, 5};
+
         String result;
         System.out.println("Welcome to the Movie Store");
 
@@ -19,7 +13,7 @@ public class Program {
 
         for(int i = 0; i < 2; i++){
             Movie m = new Movie("movie" + (i + 1), (i+1));
-            Rental r = new Rental(m, (i < 1? 10 : 5));
+            Rental r = new Rental(m, numbers[i]);
             c1.addRental(r);
 
         }
